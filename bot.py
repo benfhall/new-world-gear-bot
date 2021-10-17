4022940f65ade4e4a7a9917b346d7d841287bb15
Ben Hall <swidexx@gmail.com>
2021-10-17 16:49:54 -0500

    im so dumb

--- bot.py
+++ bot.py
@@ -39,12 +39,12 @@ async def gear(ctx, target: discord.User=None):
     embed.add_field(name="Level",value=str(await database.pull_by_index(index, "level")).title())
     embed.add_field(name="Gear Score",value=str(await database.pull_by_index(index, "gs")).title())
     embed.add_field(name="Primary Weapon",value=str(await database.pull_by_index(index, "primary")).title())
-    img_link = str(await database.pull_by_index(index, "secondary")).title()
-    if validators.url(img_link) and "http" in img_link:
-        embed.add_field(name="Secondary Weapon",value=img_link)
+    embed.add_field(name="Secondary Weapon",value= str(await database.pull_by_index(index, "secondary")).title())
 
-    embed.set_image(url=str(await database.pull_by_index(index, "img")))
     embed.set_thumbnail(url=target.avatar_url)
+    img_link = str(await database.pull_by_index(index, "img"))
+    if validators.url(img_link):
+        embed.set_image(url=img_link)
 
     embed.set_footer(text="Last Updated: " + str(await database.pull_by_index(index, "date")))
 
