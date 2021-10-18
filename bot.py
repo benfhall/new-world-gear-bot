1586616a0d67c5c8d09f20e2d1fb1c5428862b25
Ben Hall <swidexx@gmail.com>
2021-10-17 19:26:28 -0500

    bade more obvious

--- bot.py
+++ bot.py
@@ -34,8 +34,6 @@ async def gear(ctx, target: discord.User=None):
         await ctx.channel.send("```" + target.name + " was not found in the gear database.```")
 
     # create return embed
-    
-
     embed = discord.Embed(
         title=target.name + "'s Gear",
         color=fac_color.get(str(await database.pull_by_index(index, "faction")).lower(),0x000000)
@@ -66,7 +64,7 @@ async def text_res(member, question):
         try:
             msg = await bot.wait_for('message', check = lambda x: x.channel == member.dm_channel and x.author == member, timeout=60)
         except asyncio.TimeoutError:
-            return ""
+            return
         finally:
             return msg.content
 
@@ -78,17 +76,18 @@ async def update(ctx, field=None):
     except ValueError:
         field = None
         await ctx.author.send("Not found in database, running entire update process.")
+    await ctx.channel.send("If you make any errors, you can go back anytime after by doing `!update [field]`.\nThe field will be listed after the question around brackets, [field].")
 
     # update name
     await database.push(ctx.author.id, "name", ctx.author.name)
 
     if field == None or str(field) == "ign":
-        res = await text_res(ctx.author,'```What is your in-game name?```')
+        res = await text_res(ctx.author,'```What is your in-game name? [ign]```')
         await database.push(ctx.author.id, "ign", res)
 
     # set faction
     if field == None or str(field) == "faction":
-        res = await text_res(ctx.author,'```What faction are you in?```')
+        res = await text_res(ctx.author,'```What faction are you in? (covenant/marauder/syndicate) [faction]```')
         if res.lower() in factions:
             await database.push(ctx.author.id, "faction", res)
         else:
@@ -96,12 +95,12 @@ async def update(ctx, field=None):
 
     # set company
     if field == None or str(field) == "company":
-        res = await text_res(ctx.author,'```What is the name of your company in game?```')
+        res = await text_res(ctx.author,'```What is the name of your company in game? [company]```')
         await database.push(ctx.author.id, "company", res)
 
     # set lvl
     if field == None or str(field) == "lvl":
-        res = await text_res(ctx.author,'```What level is your character currently in New World? (1-60)```')
+        res = await text_res(ctx.author,'```What level is your character currently in New World? [lvl]```')
         try:
             res = int(res)
             if res >= 1 and res <= 60:
@@ -113,7 +112,7 @@ async def update(ctx, field=None):
 
     # set gs
     if field == None or str(field) == "gs":
-        res = await text_res(ctx.author,'```What gear score is your character currently in New World? (1-600)```')
+        res = await text_res(ctx.author,'```What gear score is your character currently in New World? [gs]```')
         try:
             res = int(res)
             if res >= 1 and res <= 600:
@@ -125,7 +124,7 @@ async def update(ctx, field=None):
 
     # set armor type
     if field == None or str(field) == "armor":
-        res = str(await text_res(ctx.author,'```What is the weight of your armor? (i.e light, medium, heavy)```')).lower()
+        res = str(await text_res(ctx.author,'```What is the weight of your armor? (i.e light, medium, heavy) [armor]```')).lower()
         if res in armor_types:
             await database.push(ctx.author.id, "armor", res)
         else:
@@ -133,7 +132,7 @@ async def update(ctx, field=None):
 
     # set primary
     if field == None or str(field) == "primary":
-        res = str(await text_res(ctx.author,'```What is your primary weapon? (i.e fire staff, great axe, sword)```')).lower()
+        res = str(await text_res(ctx.author,'```What is your primary weapon? (i.e fire staff, great axe, sword) [primary]```')).lower()
         if res in weapon_types:
             await database.push(ctx.author.id, "primary", res)
         else:
@@ -141,7 +140,7 @@ async def update(ctx, field=None):
 
     # set secondary
     if field == None or str(field) == "secondary":
-        res = str(await text_res(ctx.author,'```What is your secondary weapon? (i.e fire staff, great axe, sword)```')).lower()
+        res = str(await text_res(ctx.author,'```What is your secondary weapon? (i.e fire staff, great axe, sword) [secondary]```')).lower()
         if res in weapon_types:
             await database.push(ctx.author.id, "secondary", res)
         else:
@@ -149,7 +148,7 @@ async def update(ctx, field=None):
 
     # set image
     if field == None or str(field) == "img":
-        res = await text_res(ctx.author,'```Please provide a link to an image of your gear, showing attributes. (link)```')
+        res = await text_res(ctx.author,'```Please provide a link to an image of your gear, showing attributes. (link) [img]```')
         if validators.url(res):
             await database.push(ctx.author.id, "img", res)
         else:
@@ -158,6 +157,8 @@ async def update(ctx, field=None):
     # set date
     today = date.today()
     await database.push(ctx.author.id, "date", str(today.strftime("%m/%d/%Y")))
+
+    await ctx.author.send("Updating process complete!")
 
 @bot.command(pass_context=True)
 async def help(ctx):
